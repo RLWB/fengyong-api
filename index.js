b0709caaf1c96bf90356c12a3dbfb780fe5732d8
rlwb <rlwb1017669154@gmail.com>
2022-10-13 18:01:53 +0800

    change port

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.port || 3002;
+app.use((req, res, next) => {
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
 require("./router")(app);
+
 app.get("/", (req, res) => {
   res.send({
     name: "hello",
